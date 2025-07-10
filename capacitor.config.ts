bf241d32d36c2c80372b7cebe59e981967d4f5ea
chuckis <chuckis94@gmail.com>
2025-07-10 19:07:05 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sinnermanIonicGame',
+  webDir: 'dist'
+};
+
+export default config;
